ad34b15ce0a335a3a3172cdefd26783b952aace1
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2026-07-22 00:13:41 -0300

    change categories page

--- app/pages/categories/index.tsx
+++ app/pages/categories/index.tsx
@@ -1,12 +1,32 @@
 import { Badge } from "@/components/ui/badge";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemGroup,
+  ItemMedia,
+  ItemTitle,
+} from "@/components/ui/item";
+import { useCompetition } from "@/context/competition";
+import type { Category } from "@/types/category";
+import { Edit, Plus, Trash2, Users } from "lucide-react";
+import { useState } from "react";
 import { CategoryModal } from "../../components/category-modal";
 import type { Route } from "../../pages/categories/+types";
 import { EmptyCategories } from "./empty";
-import { Button } from "@/components/ui/button";
-import { Edit, Plus, Trash2, Users } from "lucide-react";
-import { useCompetition } from "@/context/competition";
-import { Item, ItemActions, ItemContent, ItemDescription, ItemMedia, ItemTitle } from "@/components/ui/item";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -19,52 +39,88 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Categories() {
-  const { categories, addCategory } = useCompetition();
+  const { categories, setEditingCategory, deleteCategory } = useCompetition();
+  const [open, setOpen] = useState<boolean>(false);
 
-  return categories.length ? (
+  const onEdit = (category: Category) => {
+    setEditingCategory(category);
+    setOpen(true);
+  };
+
+  return (
     <>
-      <CategoryModal
-        handleSubmit={addCategory}
-        render={
-          <Button>
+      {console.log(categories)}
+      {categories.length ? (
+        <>
+          <Button onClick={() => setOpen(true)}>
             <Plus /> Adicionar Modalidade
           </Button>
-        }
-      />
-      <div className="my-2 flex flex-col gap-4">
-        {categories?.map((category) => (
-          <Item variant={"outline"}>
-            <ItemMedia variant={"icon"}>
-              <Users />
-            </ItemMedia>
-            <ItemContent>
-              <ItemTitle>{category.name}</ItemTitle>
-              <ItemDescription className="flex flex-wrap gap-2">
-                <Badge variant={"secondary"}>
-                  {category.competitors}{" "}
-                  {category.competitors <= 1 ? "competidor" : "comptidores"}
-                </Badge>
-                <Badge variant={"secondary"}>{category.rounds} voltas</Badge>
-              </ItemDescription>
-            </ItemContent>
-            <ItemActions>
-              <Button variant={"default"}>
-                <Edit />
-              </Button>
-              <Button variant={"destructive"}>
-                <Trash2 />
-              </Button>
-            </ItemActions>
-          </Item>
-        ))}
-      </div>
+
+          <ItemGroup className="my-2 flex flex-col gap-4">
+            {categories?.map((category, index) => (
+              <Item variant={"outline"} key={index}>
+                <ItemMedia variant={"icon"}>
+                  <Users />
+                </ItemMedia>
+                <ItemContent>
+                  <ItemTitle>{category.name}</ItemTitle>
+                  <ItemDescription className="flex flex-wrap gap-2">
+                    <Badge variant={"secondary"}>
+                      {category.competitors}{" "}
+                      {category.competitors <= 1 ? "competidor" : "comptidores"}
+                    </Badge>
+                    <Badge variant={"secondary"}>
+                      {category.rounds} voltas
+                    </Badge>
+                  </ItemDescription>
+                </ItemContent>
+                <ItemActions>
+                  <Button variant={"default"} onClick={() => onEdit(category)}>
+                    <Edit />
+                  </Button>
+                  <AlertDialog>
+                    <AlertDialogTrigger
+                      render={
+                        <Button variant={"destructive"}>
+                          <Trash2 />
+                        </Button>
+                      }
+                    />
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>
+                          Você deseja mesmo exluir essa modalidade?
+                        </AlertDialogTitle>
+                        <AlertDialogDescription>
+                          Essa ação não poderá ser desfeira. A modalidade será
+                          exluida permanentemente.
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel variant={"outline"}>
+                          Cancelar
+                        </AlertDialogCancel>
+                        <AlertDialogAction
+                          variant={"destructive"}
+                          onClick={() => deleteCategory(category.id)}
+                        >
+                          Excluir
+                        </AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                </ItemActions>
+              </Item>
+            ))}
+          </ItemGroup>
+        </>
+      ) : (
+        <EmptyCategories>
+          <Button onClick={() => setOpen(true)}>Adicionar Modalidade</Button>
+        </EmptyCategories>
+      )}
+
+      <CategoryModal open={open} onOpenChange={setOpen} />
     </>
-  ) : (
-    <EmptyCategories>
-      <CategoryModal
-        handleSubmit={addCategory}
-        render={<Button>Adicionar Modalidade</Button>}
-      />
-    </EmptyCategories>
   );
 }
