9cf285af4d90d659950068f439f58beff1a0f7ba
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2026-07-22 00:13:07 -0300

    change competition store

--- app/context/competition/index.ts
+++ app/context/competition/index.ts
@@ -1,13 +1,29 @@
-import type { CategoryType } from "@/pages/categories/schema";
+import type { Category } from "@/types/category";
 import { create } from "zustand";
 
 interface CompetitionState {
-  categories: CategoryType[];
-  addCategory: (newCategory: CategoryType) => void;
+  categories: Category[];
+  editingCategory?: Category;
+  addCategory: (newCategory: Category) => void;
+  updateCategory: (category: Category) => void;
+  deleteCategory: (id: string) => void;
+  setEditingCategory: (category?: Category) => void;
 }
 
 export const useCompetition = create<CompetitionState>()((set) => ({
   categories: [],
+  editingCategory: undefined,
   addCategory: (newCategory) =>
     set((state) => ({ categories: [...state.categories, newCategory] })),
+  updateCategory: (category) =>
+    set((state) => ({
+      categories: state.categories.map((item) =>
+        item.id === category.id ? category : item,
+      ),
+    })),
+  deleteCategory: (id) =>
+    set((state) => ({
+      categories: state.categories.filter((item) => item.id !== id),
+    })),
+  setEditingCategory: (category) => set(() => ({ editingCategory: category })),
 }));
