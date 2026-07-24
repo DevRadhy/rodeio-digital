a17567461207ea15b364ebfcf7c4b6fb96362da4
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2026-07-24 01:06:55 -0300

    change form-rounds component

--- app/components/form-category/form-rounds.tsx
+++ app/components/form-category/form-rounds.tsx
@@ -1,6 +1,7 @@
 import { X } from "lucide-react";
 import { useState } from "react";
 import {
+  Controller,
   useFormContext,
   useWatch,
   type Control,
@@ -8,7 +9,7 @@ import {
   type FieldValues,
 } from "react-hook-form";
 import { Button } from "../ui/button";
-import { Field, FieldDescription, FieldLabel } from "../ui/field";
+import { Field, FieldDescription, FieldError, FieldLabel } from "../ui/field";
 import { Input } from "../ui/input";
 
 type FormRoundsProps<T extends FieldValues> = {
@@ -20,23 +21,42 @@ export function FormRounds<T extends FieldValues>({
   control,
   name,
 }: FormRoundsProps<T>) {
-  const { setValue } = useFormContext();
+  const { setValue, watch } = useFormContext();
 
-  const rounds = useWatch({
+  const forceRounds = useWatch({
     control,
     name,
   });
 
-  console.log(rounds);
+  const rounds = watch("rounds");
+  const forces = watch("forces");
 
   const [round, setRound] = useState("");
+  const [error, setError] = useState("");
 
   const addValue = () => {
     const value = Number(round);
 
     if (!Number.isFinite(value)) return;
 
-    setValue(name, [...(rounds ?? []), value] as T[typeof name], {
+    if (value < 1 || value > rounds) {
+      setError(`O número de armadas deve estar entre 1 e ${rounds}.`);
+      return;
+    }
+
+    const valueAlreadyExists = forces.find(
+      (force: { name: string; rounds: number[] }) =>
+        force.rounds.some((round: number) => round === value),
+    );
+
+    if (valueAlreadyExists) {
+      setError(
+        `Esse valor já está sendo usando na força ${valueAlreadyExists.name}.`,
+      );
+      return;
+    }
+
+    setValue(name, [...(forceRounds ?? []), value] as T[typeof name], {
       shouldDirty: true,
       shouldTouch: true,
       shouldValidate: true,
@@ -48,7 +68,7 @@ export function FormRounds<T extends FieldValues>({
   const removeValue = (index: number) => {
     setValue(
       name,
-      (rounds ?? []).filter((_, i) => i !== index) as T[typeof name],
+      (forceRounds ?? []).filter((_, i) => i !== index) as T[typeof name],
       {
         shouldDirty: true,
         shouldTouch: true,
@@ -60,25 +80,44 @@ export function FormRounds<T extends FieldValues>({
   return (
     <div className="mt-6">
       <div className="flex gap-2">
-        <Field data-invalid={rounds.invalid}>
-          <FieldLabel htmlFor={name}>Voltas</FieldLabel>
-          <Input
-            type="number"
-            id={name}
-            autoComplete="off"
-            onChange={(e) => setRound(e.target.value)}
-            value={round}
-          />
-          <FieldDescription>Voltas de classificatória.</FieldDescription>
-        </Field>
-
-        <Button type="button" onClick={addValue} className={"self-center"}>
-          Adicionar
-        </Button>
+        <Controller
+          name={name}
+          control={control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={forceRounds.invalid}>
+              <FieldLabel htmlFor={name}>Voltas</FieldLabel>
+              <div className="grid grid-cols-[1fr_auto] gap-4">
+                <Input
+                  {...field}
+                  type="number"
+                  id={field.name}
+                  autoComplete="off"
+                  onChange={(e) => {
+                    setRound(e.target.value);
+                    setError("");
+                  }}
+                  value={round}
+                />
+                <Button
+                  type="button"
+                  onClick={addValue}
+                  className={"self-center"}
+                >
+                  Adicionar
+                </Button>
+              </div>
+              <FieldDescription>Voltas de classificatória.</FieldDescription>
+              {fieldState.invalid ||
+                (error && (
+                  <FieldError errors={[fieldState.error, { message: error }]} />
+                ))}
+            </Field>
+          )}
+        />
       </div>
 
       <div className="flex flex-wrap gap-2">
-        {rounds.map((round: number, index: number) => (
+        {forceRounds.map((round: number, index: number) => (
           <Button onClick={() => removeValue(index)} variant={"secondary"}>
             {round}
             <X />
