b33a86bc0afb237dfcb0b96d4de15550a4509eb7
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2026-07-30 20:53:49 -0300

    refactor registration page

--- app/pages/registration/index.tsx
+++ app/pages/registration/index.tsx
@@ -1,4 +1,4 @@
-import { CategoryCard } from "@/components/registrations/category-card";
+import { CategoryCard } from "@/components/categories/category-card";
 import { RegistrationDialog } from "@/components/registrations/registration-dialog";
 import { SiteHeader } from "@/components/site-header";
 import { useCategories } from "@/stores/categories";
@@ -6,7 +6,7 @@ import type { Category } from "@/types/category";
 import { useState } from "react";
 
 export default function Registration() {
-  const { categories } = useCategories();
+  const categories = useCategories((state) => state.categories);
   const [selectedCategory, setSelectedCategory] = useState<Category | null>(
     null,
   );
@@ -14,29 +14,23 @@ export default function Registration() {
   return (
     <>
       <SiteHeader />
-      <div className="flex flex-1 flex-col">
-        <div className="@container/main flex flex-1 flex-col gap-2">
-          <div className="flex flex-col gap-4 p-4 md:gap-6 md:py-6">
-            <section className="grid grid-cols-4 gap-4">
-              {categories.map((category) => (
-                <CategoryCard
-                  key={category.id}
-                  category={category}
-                  onRegister={() => setSelectedCategory(category)}
-                />
-              ))}
-            </section>
-
-            <RegistrationDialog
-              category={selectedCategory}
-              open={!!selectedCategory}
-              onOpenChange={(open) => {
-                if (!open) setSelectedCategory(null);
-              }}
-            />
-          </div>
-        </div>
+      <div className="grid grid-cols-4 gap-4 p-4">
+        {categories.map((category) => (
+          <CategoryCard
+            key={category.id}
+            category={category}
+            onRegister={() => setSelectedCategory(category)}
+          />
+        ))}
       </div>
+
+      <RegistrationDialog
+        category={selectedCategory}
+        open={!!selectedCategory}
+        onOpenChange={(open) => {
+          if (!open) setSelectedCategory(null);
+        }}
+      />
     </>
   );
 }
