a2ff76135ab4b2f567970b7c5e79d68b409f98a4
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2026-07-30 20:56:38 -0300

    change session route

--- app/routes.ts
+++ app/routes.ts
@@ -11,6 +11,6 @@ export default [
   route("/dashboard", "pages/dashboard/index.tsx"),
   ...prefix("/competition", [
     index("pages/competition/index.tsx"),
-    route(":categoryId", "pages/competition/run.tsx"),
+    route(":categoryId", "pages/competition/session.tsx"),
   ]),
 ] satisfies RouteConfig;
