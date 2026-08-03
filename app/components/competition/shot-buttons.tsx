6c77959f6d02c0b39d1906beb7a5a095cc1a8eb1
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2026-08-03 19:20:16 -0300

    add setShot and disabled props to shot-buttons component

--- app/components/competition/shot-buttons.tsx
+++ app/components/competition/shot-buttons.tsx
@@ -1,28 +1,42 @@
 import { Check, X } from "lucide-react";
 import { Button } from "../ui/button";
-import type { Shot } from "@/types/qualification";
+import type { Shot } from "@/types/competition";
 
 interface ShotButtonProps {
   value: Shot | null;
+  setShot: (value: Shot) => void;
+  disabled?: boolean;
 }
 
-export function ShotButtons({ value }: ShotButtonProps) {
+export function ShotButtons({
+  value,
+  setShot,
+  disabled = false,
+}: ShotButtonProps) {
+  const onSetShot = (shot: boolean) => {
+    setShot(shot === value ? null : shot);
+  };
+
   return (
     <div className="flex gap-2">
       <Button
         variant={"outline"}
-        className={`rounded-sm font-bold
-          ${value === true ? "bg-emerald-500 border-emerald-400 text-slate-950" : "border-slate-700 text-slate-500 hover:bg-emerald-400 hover:border-emerald-300 hover:text-emerald-100"}
+        disabled={disabled}
+        onClick={() => onSetShot(true)}
+        className={`${value === true ? "bg-emerald-500 outline-emerald-400 text-emerald-100" : "outline-slate-400 text-slate-400 "}
+          rounded-sm font-bold w-12 h-10 px-3.5 flex justify-center items-center outline
+          hover:bg-emerald-400 hover:outline-emerald-300 hover:text-emerald-100
         `}
-        size={"lg"}
       >
         <Check />
       </Button>
       <Button
         variant={"outline"}
-        size={"lg"}
-        className={`rounded-sm font-bold
-          ${value === false ? "bg-rose-500 border-rose-400 text-slate-950" : "border-slate-700 text-slate-500 hover:bg-rose-400 hover:border-rose-300 hover:text-rose-100"} 
+        disabled={disabled}
+        onClick={() => onSetShot(false)}
+        className={`${value === false ? "bg-rose-500 outline-rose-400 text-rose-100" : "outline-slate-400 text-slate-400"} 
+          rounded-sm font-bold w-12 h-10 px-3.5 flex justify-center items-center outline 
+          hover:bg-rose-400 hover:outline-rose-300 hover:text-rose-100
         `}
       >
         <X />
