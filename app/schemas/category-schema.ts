0fa2e131a84661e154531a31fcd9c05a42d361e4
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2026-08-04 11:00:13 -0300

    refactor category schema

--- app/schemas/category-schema.ts
+++ app/schemas/category-schema.ts
@@ -6,7 +6,7 @@ export const CategorySchema = z
       .string("Você precisa informar o nome da modalidade.")
       .min(3, "O nome da modalidade precisa conter pelo menos 3 caracteres.")
       .max(32, "O tamanho máximo para o nome é de 32 caracteres."),
-    competitors: z
+    competitorsPerRegistration: z
       .number("Você precisa informar o número de competidores por inscrição.")
       .positive("O número precisa ser maior que 0.")
       .min(
@@ -17,52 +17,55 @@ export const CategorySchema = z
         50,
         "O número máximo de competidores por inscrição é de 50 competidores.",
       ),
-    rounds: z
-      .number("Você precisa informar o número de voltas de classificatória.")
-      .positive("O número precisar ser maior que 0.")
-      .min(1, "O número mínimo de voltas não pode ser menor que 1.")
-      .max(100, "O número máximo de voltas não pode ser maior 100."),
-    price: z
+
+    pricePerRegistration: z
       .float32("Você precisa informar o valor da inscriçao.")
       .min(0, "O valor minimo de incrição não pode ser menor que R$0,00.")
       .max(99999, "Valor de inscrição inválido."),
-    isDuel: z.boolean(),
-    forces: z.array(
-      z.object({
-        id: z.string(),
-        name: z
-          .string("Você precisa informar o nome da força.")
-          .max(24, "O tamanho máximo para o nome é de 24 caracteres."),
-        qualifyingScores: z.array(
-          z
-            .number(
-              "Você precisa informar o número de voltas de classificatória da força.",
-            )
-            .positive("O número precisar ser maior que 0.")
-            .min(1, "O número mínimo de voltas não pode ser menor que 1.")
-            .max(100, "O número máximo de voltas não pode ser maior 100."),
-        ),
-      }),
-    ),
+    duel: z.boolean(),
+    qualification: z.object({
+      rounds: z
+        .number("Você precisa informar o número de voltas de classificatória.")
+        .positive("O número precisar ser maior que 0.")
+        .min(1, "O número mínimo de voltas não pode ser menor que 1.")
+        .max(100, "O número máximo de voltas não pode ser maior 100."),
+      groups: z.array(
+        z.object({
+          id: z.string(),
+          name: z
+            .string("Você precisa informar o nome da força.")
+            .max(24, "O tamanho máximo para o nome é de 24 caracteres."),
+          qualifyingScores: z.array(
+            z
+              .number(
+                "Você precisa informar o número de voltas de classificatória da força.",
+              )
+              .positive("O número precisar ser maior que 0.")
+              .min(1, "O número mínimo de voltas não pode ser menor que 1.")
+              .max(100, "O número máximo de voltas não pode ser maior 100."),
+          ),
+        }),
+      ),
+    }),
   })
   .superRefine((data, ctx) => {
-    if (!data.isDuel) return;
+    if (!data.duel) return;
 
     const usedRounds = new Map<number, number>();
 
-    data.forces.forEach((force, forceIndex) => {
-      if (force.qualifyingScores.length === 0) {
+    data.qualification.groups.forEach((group, forceIndex) => {
+      if (group.qualifyingScores.length === 0) {
         ctx.addIssue({
           code: "custom",
-          message: `A força ${force.name} deve possuir pelo menos uma volta de classificatória.`,
+          message: `A força ${group.name} deve possuir pelo menos uma volta de classificatória.`,
         });
       }
 
-      force.qualifyingScores.forEach((round, roundIndex) => {
-        if (round < 1 || round > data.rounds) {
+      group.qualifyingScores.forEach((round, roundIndex) => {
+        if (round < 1 || round > data.qualification.rounds) {
           ctx.addIssue({
             code: "custom",
-            message: `As voltas de classificatória da força ${force.name} devem estar entre 1 e ${data.rounds}.`,
+            message: `As voltas de classificatória da força ${group.name} devem estar entre 1 e ${data.qualification.rounds}.`,
           });
         }
 
