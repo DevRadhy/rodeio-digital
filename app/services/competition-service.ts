89a8762a6145c3a42d49bac7d28ca35ca39d0a15
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2026-08-04 10:47:50 -0300

    refactor competition service

--- app/services/competition-service.ts
+++ app/services/competition-service.ts
@@ -6,229 +6,200 @@ import type {
   Shot,
 } from "@/types/competition";
 import type { Registration } from "@/types/registration";
+import { everyPositive, getRegistrationChuncks } from "@/utils";
 import { v4 } from "uuid";
 
-function getRegistrationChuncks(registrations: Registration[], length: number) {
-  const chunks = [];
-
-  for (let i = 0; i < registrations.length; i += length) {
-    chunks.push(registrations.slice(i, i + length));
-  }
-
-  return chunks;
-}
-
-function create(
-  category: Category,
-  registrations: Registration[],
-): Competition {
-  const registrationsChunks = getRegistrationChuncks(registrations, 5);
-
-  return {
-    categoryId: category.id,
-    phase: "qualification",
-    status: "running",
-    groups: registrationsChunks.map((registrations, index) => ({
-      id: v4(),
-      currentRound: 1,
-      name: `Pelotão ${index + 1}`,
-      status: "running",
-      registrations: registrations,
-      rounds: Array.from({ length: category.rounds }, () => null).map(
-        (_, round) => ({
-          number: round + 1,
-          results: registrations.map((registration) => ({
-            registrationId: registration.id,
-            competitors: registration.competitors.map((competitor) => ({
-              competitorId: competitor.id,
-              shot: null,
-            })),
-          })),
-        }),
-      ),
-    })),
-  };
-}
-
-function updateShot(
-  group: CompetitionGroup,
-  registrationId: string,
-  competitorId: string,
-  shot: Shot,
-): CompetitionGroup {
-  const rounds = group.rounds.map((round) => {
-    if (round.number !== group.currentRound) {
-      return round;
-    }
+export class CompetitionService {
+  static create(
+    category: Category,
+    registrations: Registration[],
+  ): Competition {
+    const registrationsChunks = getRegistrationChuncks(registrations, 5);
 
     return {
-      ...round,
-      results: round.results.map((result) => {
-        if (result.registrationId !== registrationId) {
-          return result;
-        }
-
-        return {
-          ...result,
-          competitors: result.competitors.map((competitor) => {
-            if (competitor.competitorId !== competitorId) {
-              return competitor;
-            }
-
-            return {
-              ...competitor,
-              shot,
-            };
+      categoryId: category.id,
+      phase: "qualification",
+      status: "running",
+      groups: registrationsChunks.map((registrations, index) => ({
+        id: v4(),
+        currentRound: 1,
+        name: `Pelotão ${index + 1}`,
+        status: "running",
+        registrations: registrations,
+        rounds: Array.from({ length: category.rounds }, () => null).map(
+          (_, round) => ({
+            number: round + 1,
+            results: registrations.map((registration) => ({
+              registrationId: registration.id,
+              competitors: registration.competitors.map((competitor) => ({
+                competitorId: competitor.id,
+                shot: null,
+              })),
+            })),
           }),
-        };
-      }),
+        ),
+      })),
     };
-  });
+  }
 
-  return {
-    ...group,
-    rounds: rounds,
-  };
-}
-
-function nextRound(
-  competition: Competition,
-  group: CompetitionGroup,
-  category: Category,
-): Competition {
-  const results = group.rounds[group.currentRound - 1].results;
-  const isCompetitionFinished = isFinished(results);
-
-  return {
-    ...competition,
-    groups: competition.groups.map((g) => {
-      if (g.id !== group.id) {
-        return g;
+  static updateShot(
+    group: CompetitionGroup,
+    registrationId: string,
+    competitorId: string,
+    shot: Shot,
+  ): CompetitionGroup {
+    const rounds = group.rounds.map((round) => {
+      if (round.number !== group.currentRound) {
+        return round;
       }
 
-      if (competition.phase === "qualification") {
-        if (group.currentRound === category.rounds) {
+      return {
+        ...round,
+        results: round.results.map((result) => {
+          if (result.registrationId !== registrationId) {
+            return result;
+          }
+
+          return {
+            ...result,
+            competitors: result.competitors.map((competitor) => {
+              if (competitor.competitorId !== competitorId) {
+                return competitor;
+              }
+
+              return {
+                ...competitor,
+                shot,
+              };
+            }),
+          };
+        }),
+      };
+    });
+
+    return {
+      ...group,
+      rounds: rounds,
+    };
+  }
+
+  static nextRound(
+    competition: Competition,
+    group: CompetitionGroup,
+    category: Category,
+  ): Competition {
+    const results = group.rounds[group.currentRound - 1].results;
+
+    return {
+      ...competition,
+      groups: competition.groups.map((g) => {
+        if (g.id !== group.id) {
+          return g;
+        }
+
+        if (competition.phase === "qualification") {
+          if (group.currentRound === category.rounds) {
+            return {
+              ...group,
+              currentRound: group.currentRound,
+              status: "finished",
+            };
+          }
+
           return {
             ...group,
-            currentRound: group.currentRound,
-            status: "finished",
+            currentRound: group.currentRound + 1,
           };
         }
 
         return {
           ...group,
           currentRound: group.currentRound + 1,
-        };
-      }
-
-      return {
-        ...group,
-        currentRound: group.currentRound + 1,
-        status: isCompetitionFinished ? "finished" : "running",
-        rounds: [
-          ...group.rounds,
-          {
-            number: group.currentRound + 1,
-            results: results.filter(everyPositive).map((result) => ({
-              registrationId: result.registrationId,
-              competitors: result.competitors.map((competitor) => ({
-                ...competitor,
-                shot: null,
+          rounds: [
+            ...group.rounds,
+            {
+              number: group.currentRound + 1,
+              results: results.filter(everyPositive).map((result) => ({
+                registrationId: result.registrationId,
+                competitors: result.competitors.map((competitor) => ({
+                  ...competitor,
+                  shot: null,
+                })),
               })),
-            })),
-          },
-        ],
-      };
-    }),
-  };
-}
-
-function isFinished(results: CompetitionResult[]) {
-  return results.length <= 1;
-}
-
-function getCurrentRound(group: CompetitionGroup) {
-  return group.rounds.at(-1);
-}
-
-function everyPositive(result: CompetitionResult) {
-  return result.competitors.every((competitor) => competitor.shot);
-}
-
-function addRegistration(
-  competition: Competition,
-  category: Category,
-  registration: Registration,
-): Competition {
-  if (competition.phase !== "qualification") return competition;
-
-  const availableGroup = competition.groups.find(
-    (group) => group.currentRound <= 1 && group.registrations.length < 5,
-  );
-
-  if (availableGroup) {
-    return {
-      ...competition,
-      groups: competition.groups.map((group) => {
-        if (group.id !== availableGroup.id) {
-          return group;
-        }
-
-        return {
-          ...group,
-          registrations: [...group.registrations, registration],
-          rounds: Array.from({ length: category.rounds }, () => null).map(
-            (_, round) => ({
-              number: round + 1,
-              results: [...group.registrations, registration].map(
-                (registration) => ({
-                  registrationId: registration.id,
-                  competitors: registration.competitors.map((competitor) => ({
-                    competitorId: competitor.id,
-                    shot: null,
-                  })),
-                }),
-              ),
-            }),
-          ),
+            },
+          ],
         };
       }),
     };
   }
 
-  return {
-    ...competition,
-    groups: [
-      ...competition.groups,
-      {
-        id: v4(),
-        name: `Pelotão ${competition.groups.length + 1}`,
-        currentRound: 1,
-        registrations: [registration],
-        status: "running",
-        rounds: Array.from({ length: category.rounds }, () => null).map(
-          (_, round) => ({
-            number: round + 1,
-            results: [
-              {
-                registrationId: registration.id,
-                competitors: registration.competitors.map((competitor) => ({
-                  competitorId: competitor.id,
-                  shot: null,
-                })),
-              },
-            ],
-          }),
-        ),
-      },
-    ],
-  };
-}
+  static addRegistration(
+    competition: Competition,
+    category: Category,
+    registration: Registration,
+  ): Competition {
+    if (competition.phase !== "qualification") return competition;
 
-export const CompetitionService = {
-  create,
-  updateShot,
-  nextRound,
-  isFinished,
-  addRegistration,
-};
+    const availableGroup = competition.groups.find(
+      (group) => group.currentRound <= 1 && group.registrations.length < 5,
+    );
+
+    if (availableGroup) {
+      return {
+        ...competition,
+        groups: competition.groups.map((group) => {
+          if (group.id !== availableGroup.id) {
+            return group;
+          }
+
+          return {
+            ...group,
+            registrations: [...group.registrations, registration],
+            rounds: Array.from({ length: category.rounds }, () => null).map(
+              (_, round) => ({
+                number: round + 1,
+                results: [...group.registrations, registration].map(
+                  (registration) => ({
+                    registrationId: registration.id,
+                    competitors: registration.competitors.map((competitor) => ({
+                      competitorId: competitor.id,
+                      shot: null,
+                    })),
+                  }),
+                ),
+              }),
+            ),
+          };
+        }),
+      };
+    }
+
+    return {
+      ...competition,
+      groups: [
+        ...competition.groups,
+        {
+          id: v4(),
+          name: `Pelotão ${competition.groups.length + 1}`,
+          currentRound: 1,
+          registrations: [registration],
+          status: "running",
+          rounds: Array.from({ length: category.rounds }, () => null).map(
+            (_, round) => ({
+              number: round + 1,
+              results: [
+                {
+                  registrationId: registration.id,
+                  competitors: registration.competitors.map((competitor) => ({
+                    competitorId: competitor.id,
+                    shot: null,
+                  })),
+                },
+              ],
+            }),
+          ),
+        },
+      ],
+    };
+  }
+}
--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,3 +1,6 @@
+import type { CompetitionResult } from "@/types/competition";
+import type { Registration } from "@/types/registration";
+
 export const formatNumber = (value: number, digits = 2) => {
   return Intl.NumberFormat("pt-BR", {
     style: "decimal",
@@ -10,4 +13,21 @@ export const formatCurrency = (value: number) => {
     style: "currency",
     currency: "BRL",
   }).format(value);
+};
+
+export const getRegistrationChuncks = (
+  registrations: Registration[],
+  length: number,
+) => {
+  const chunks = [];
+
+  for (let i = 0; i < registrations.length; i += length) {
+    chunks.push(registrations.slice(i, i + length));
+  }
+
+  return chunks;
+};
+
+export const everyPositive = (result: CompetitionResult) => {
+  return result.competitors.every((competitor) => competitor.shot);
 };
