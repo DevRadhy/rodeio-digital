1bf5e85984b0b87b121c7b0af9b70d5d55cf788f
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2026-08-05 23:53:05 -0300

    change competitors list to show by rounds

--- app/components/competition/competition-list.tsx
+++ app/components/competition/competition-list.tsx
@@ -8,10 +8,12 @@ interface CompetitionListProps {
 export function CompetitionList({ group }: CompetitionListProps) {
   return (
     <div className="flex flex-col justify-center gap-4 py-8">
-      {group.registrations.map((registration) => (
+      {group.rounds[group.currentRound - 1].results.map((result) => (
         <RegistrationCard
-          key={registration.id}
-          registration={registration}
+          key={result.registrationId}
+          registration={group.registrations.find(
+            (r) => r.id === result.registrationId,
+          )!}
           group={group}
         />
       ))}
