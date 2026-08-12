f47969292f537c43e719688370170067e164452d
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2026-08-12 20:19:55 -0300

    fix: remove dashboard route

--- app/routes.ts
+++ app/routes.ts
@@ -10,7 +10,6 @@ export default [
   layout("./layouts/pages/layout.tsx", [
     route("/categories", "pages/categories/index.tsx"),
     route("/registrations", "pages/registration/index.tsx"),
-    route("/dashboard", "pages/dashboard/index.tsx"),
     ...prefix("/competition", [
       index("pages/competition/index.tsx"),
       route(":categoryId", "pages/competition/session.tsx"),
